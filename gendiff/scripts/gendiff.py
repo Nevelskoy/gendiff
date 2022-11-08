bee61609fb6d17bfeb8d1ac2c0ab6f7d6a5e3595
Nevelskoy <safelikc@gmail.com>
2022-11-08 07:14:57 +0300

    init

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+DESCRIPTION = 'Compares two configuration files and shows a difference.'
+
+
+def main():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', dest='format', help='format of output')
+
+    
+    args = parser.parse_args()
+    print(args.first_file, args.second_file, args.format)
+
+
+if __name__ == '__main__':
+    main()
+    
