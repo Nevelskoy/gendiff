97c844b641b6e2b5f1fff6d643539a82bb0c1d4b
Nevelskoy <safelikc@gmail.com>
2022-11-15 18:49:31 +0300

    fix diff

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,23 +1,26 @@
+def get_diff(first, second, key):
+    if isinstance(first.get(key), dict) and isinstance(second.get(key), dict):
+        return
+      
+    if key not in first:
+        return ('added', key, second.get(key))
+    elif key not in second:
+        return ('deleted', key, first.get(key))
+    elif first.get(key) == second.get(key):
+        return ('unchanged', key, first.get(key))
+    else: 
+        return ('changed', key, (first.get(key), second.get(key)))
 
 
-def diff_data(first_data, second_data):
-    '''get diff structure'''
-    #обход структуры через рекурсию, потому что вложенный json файл имеет рекурсивную природу.
-
-
-
-def get_diff(first, second):
-    '''get diff dictonary'''
+def get_diff_data(first, second):
+    result = []
     keys = first.keys() | second.keys()
-    result = {}
     for key in sorted(keys):
-        if key not in first:
-            result[key] = 'added'
-        elif key not in second:
-            result[key] = 'deleted'
-        elif first[key] == second[key]:
-            result[key] = 'unchanged'
+        first_value = first.get(key)
+        second_value = second.get(key)
+        if isinstance(first_value, dict) and isinstance(second_value, dict):
+            nested_dictionary = get_diff_data(first_value, second_value)
+            result.append(('dictionary', key, nested_dictionary))
         else:
-            result[key] = 'changed'
+            result.append(get_diff(first, second, key))
     return result
-
